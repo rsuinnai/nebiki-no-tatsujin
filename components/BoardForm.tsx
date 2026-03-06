621f631bb2f97ca73e8fe6dbcec45d87e284e496
rsuinnai <rsuinaixiang@gmail.com>
2026-03-06 18:00:31 +0900

    fix supabase path

--- components/BoardForm.tsx
+++ components/BoardForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../src/lib/supabase";
 
 type Props = {
   reportId: number;
